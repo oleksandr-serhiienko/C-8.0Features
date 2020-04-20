a9b81652d32dcf6c61f34b0af25bd9737dd9834b
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-20 17:09:08 +0200

    Tested new decalretions

---
+++ Demo8/4-UsingDeclarations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Demo8
+{
+    public static class UsingDeclarations
+    {
+        public static int ConvertFiles()
+        {
+            int output = 0;
+
+            using var inputFile = new StreamReader(@"C:\Users\Sasha\Downloads\Sample.txt");
+            using var outputFile = new StreamWriter(@"C:\Users\Sasha\Downloads\SampleOutput.txt");
+            
+            string line;
+            
+            while ((line = inputFile.ReadLine()) != null)
+            {
+                outputFile.Write(line);
+                output += 1;
+            }
+
+            //using (var inputfile = new StreamReader(@"‪C:\Users\Sasha\Downloads\Sample.txt"))
+            //{
+            //    using (var outputFile = new StreamWriter(@"‪C:\Users\Sasha\Downloads\SampleOutput.txt"))
+            //    {
+            //        string line;
+            //        while ((line = inputfile.ReadLine()) != null)
+            //        {
+            //            outputFile.Write(line);
+            //            output += 1;
+            //        }
+            //    }
+            //}
+            return output;
+        }
+    }
+}
