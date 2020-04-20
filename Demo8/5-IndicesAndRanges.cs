d8d30e102bb42fc27366315be12e0b4e2da72e69
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-20 17:09:27 +0200

    Tested ranges

---
+++ Demo8/5-IndicesAndRanges.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo8
+{
+    public static class IndicesAndRanges
+    {
+        public static void Demo ()
+        {
+            var places = new string[] { "first", "second", "third", "forth", "fifth" };
+
+            Console.WriteLine($"The last item is {places[^1]}");
+            Console.WriteLine($"The second last item is {places[^2]}");
+
+            foreach (var item in places[1..^2])
+            {
+                Console.WriteLine(item);                
+            } 
+        }
+    }
+}
