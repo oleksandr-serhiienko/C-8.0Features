87b35279372873dbb5b7bb8adac53ccf1fc37d48
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-20 17:07:25 +0200

    added the programm

---
+++ Demo8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Demo8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ISubTatal cart2 = new ShoppingCart();
+            cart2.CalculateSubTotal();
+            IShoppingCart cart1 = new BetterShoppingCart();
+            cart1.CalculateSubTotal();
+
+            Console.ReadLine();
+        }
+    }
+}
