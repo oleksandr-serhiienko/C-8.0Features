8c26714c8f52019d1fd3ebff8dad0c61f506ee6b
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-20 17:09:51 +0200

    tested null asighment

---
+++ Demo8/6-NullCoalescingAssigment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo8
+{
+    public static class NullCoalescingAssigment
+    {
+        public static void Deme()
+        {
+            var example = new ListDemo();
+
+            //if (example.LuckyNumbers == null)
+            //{
+            //    example.LuckyNumbers = new List<int>();
+            //}
+
+            example.LuckyNumbers ??= new List<int>();
+
+            example.LuckyNumbers.Add(27);
+            foreach (var item in example.LuckyNumbers)
+            {
+                Console.WriteLine($"Lucky Number: {item}");
+            }
+        }
+    }
+
+    public class ListDemo
+    {
+        public List<int> LuckyNumbers { get; set; }
+    }
+}
