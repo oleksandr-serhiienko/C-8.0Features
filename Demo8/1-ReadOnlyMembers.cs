18167f164904d4db4caf074c6ab4275d93b55751
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-20 17:07:55 +0200

    tested read only member in the structures

---
+++ Demo8/1-ReadOnlyMembers.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo8
+{
+    public struct Rectangle
+    {
+        public double Length { get; set; }
+
+        private double height;
+
+        public double Height
+        {
+            readonly get 
+            { 
+                return height;
+            }
+
+            set { height = value; }
+        }
+
+
+        public readonly double Area()
+        {
+            return Length * Height;
+        }
+    }
+}
